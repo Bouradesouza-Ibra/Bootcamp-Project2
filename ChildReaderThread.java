5026bc96ba6e5e6cbdf025c5d6f16130516378b5
Ibrahima Coulibaly17 <ibracly51@gmail.com>
2025-07-20 17:02:09 -0400

    Update ChildReaderThread.java

--- ChildReaderThread.java
+++ ChildReaderThread.java
@@ -1,36 +1,25 @@
-package Clientorders;
+package clientorders;
 
 import java.util.List;
 
-
+// Thread class to read child orders in the background
 public class ChildReaderThread extends Thread {
-
-    private List<ChildOrder> children;
-
+    private List<ChildOrder> children;  // Stores the list of child orders
 
     @Override
-
     public void run() {
-
         try {
-
+            // Read child orders from the CSV file
             children = ChildOrderReader.readChildOrders("child_orders.csv");
-
-            System.out.println(" Finished reading child orders");
-
+            System.out.println("Finished reading child orders");
         } catch (Exception e) {
-
-            System.out.println(" Error reading child orders: " + e.getMessage());
-
+            // Print error if reading fails
+            System.out.println("Error reading child orders: " + e.getMessage());
         }
-
     }
 
-
+    // Getter to return the list of child orders
     public List<ChildOrder> getChildren() {
-
         return children;
-
     }
-
 }
