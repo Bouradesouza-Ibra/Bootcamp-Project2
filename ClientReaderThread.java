7717baedc655b152b3e734ac3526a1076c2c2bab
Ibrahima Coulibaly17 <ibracly51@gmail.com>
2025-07-20 17:13:33 -0400

    Update ClientReaderThread.java

--- ClientReaderThread.java
+++ ClientReaderThread.java
@@ -1,36 +1,19 @@
-package Clientorders;
-
+ package clientorders;
 import java.util.List;
-
-
+// This thread reads client orders from the CSV file
 public class ClientReaderThread extends Thread {
-
-    private List<ClientOrder> clients;
-
-
+    private List<ClientOrder> clients;  // Stores the list of client orders
     @Override
-
     public void run() {
-
         try {
-
+            // Read client orders from the CSV file
             clients = ClientOrderReader.readClientOrders("client_orders.csv");
-
-            System.out.println(" Finished reading client orders");
-
+            System.out.println("Finished reading client orders");
         } catch (Exception e) {
-
-            System.out.println(" Error reading client orders: " + e.getMessage());
-
-        }
-
-    }
-
-
+            // Show error if reading fails
+            System.out.println("Error reading client orders: " + e.getMessage());
+        } }
+    // Method to return the list of client orders
     public List<ClientOrder> getClients() {
-
         return clients;
-
-    }
-
-}
+   }}
