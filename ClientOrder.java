736f6e2fb7f0161acc057128851ab237baeb55fc
Ibrahima Coulibaly17 <ibracly51@gmail.com>
2025-07-20 17:04:27 -0400

    Update ClientOrder.java

--- ClientOrder.java
+++ ClientOrder.java
@@ -1,45 +1,25 @@
-package Clientorders;
+package clientorders;
 
 public class ClientOrder {
-
     // These are the columns in the CSV
-
     String clientOrderId;
-
     String symbol;
-
     String side;
-
     int quantity;
 
-
     // Constructor 
-
     public ClientOrder(String clientOrderId, String symbol, String side, int quantity) {
-
         this.clientOrderId = clientOrderId;
-
         this.symbol = symbol;
-
         this.side = side;
-
         this.quantity = quantity;
-
     }
-
 
     // This method will help us print it nicely
-
     public String toString() {
-
         return "Client Order " + clientOrderId + 
-
                " | Symbol: " + symbol + 
-
                " | Side: " + side + 
-
                " | Quantity: " + quantity;
-
     }
-
 }
