044ab4d8b08661c1dc1c3a936bf4426f67213e16
Ibrahima Coulibaly17 <ibracly51@gmail.com>
2025-07-20 17:14:21 -0400

    Update InvalidOrderException.java

--- InvalidOrderException.java
+++ InvalidOrderException.java
@@ -1,12 +1,10 @@
-package Clientorders;
+package clientorders;
 
-@SuppressWarnings("serial")
+// This is a custom exception for invalid orders
 public class InvalidOrderException extends Exception {
 
+    // Constructor that sends the error message to the parent class (Exception)
     public InvalidOrderException(String message) {
-
-        super(message);
-
+        super(message);  // Pass the message to the Exception class
     }
-
 }
